596cc54761ff68f9fbc24785aec2ec2a00e53554
Vik-11 <vikkuznetsov11@gmail.com>
2025-02-25 18:28:57 +0500

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+
+printf "Enter natural number: "
+let n = int(Console.ReadLine())
+
+let countEven N = 
+    let rec loop numb count =
+        if numb = 0 then count
+        else
+            let digit = numb % 10
+            let newCount = 
+                if digit % 2 = 0 then count + 1 
+                else count
+            loop (numb / 10) newCount
+    loop N 0
+
+let res = countEven n
+printfn "Quantity of even digits in number %d: %d" n res
