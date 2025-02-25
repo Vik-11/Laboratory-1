0a098e7e6e0737da7345c7a6993f4183e01c1d21
Vik-11 <vikkuznetsov11@gmail.com>
2025-02-25 18:27:05 +0500

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+let readL () =
+    printf "Enter numbers with space between: "
+    let inp = (Console.ReadLine())
+    inp.Split(' ')
+    |> Array.map int
+    |> List.ofArray
+    |> List.rev
+
+let numb = readL()
+printfn "List of numbers in reverse order: %A" numb
+
